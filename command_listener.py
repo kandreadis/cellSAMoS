4c638ea30826b85f0a09176e7c30a28bb8e8b644
kandreadis <k.andreadis@kpnmail.nl>
2023-12-15 13:22:24 +0100

    continuous listening to new bash tasks!

--- command_listener.py
+++ command_listener.py
@@ -9,11 +9,16 @@ from scripts.communication_handler import print_log
 import subprocess
 
 if __name__ == "__main__":
-    listen_path = system_paths["listen_dir"]
-    bash_tasks = [os.path.join(listen_path, f) for f in os.listdir(listen_path) if f.endswith(".sh")]
-    print_log(f"---------- Found task {bash_tasks} in {listen_path} ----------")
-    for task in bash_tasks:
-        print_log(f"=> Executing task {task}...")
-        with open(task, 'rb') as file:
-            script = file.read()
-        subprocess.call(script, shell=True)
+    completed_tasks = []
+    while True:
+        listen_path = system_paths["listen_dir"]
+        bash_tasks = [os.path.join(listen_path, f) for f in os.listdir(listen_path) if f.endswith(".sh")]
+        print(f"---------- Found task {bash_tasks} in {listen_path} ----------")
+        for task in bash_tasks:
+            if not (task in completed_tasks):
+                print_log(f"=> Executing task {task}...")
+                with open(task, 'rb') as file:
+                    script = file.read()
+                subprocess.call(script, shell=True)
+                completed_tasks.append(task)
+        print(f"Completed tasks: {completed_tasks}")
